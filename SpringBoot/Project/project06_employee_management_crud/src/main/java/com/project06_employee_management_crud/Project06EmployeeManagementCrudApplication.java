913890578e39b832a0e5feaca37d171c85fb5f2e
PranjalKumar09 <coderkumarshukla@gmail.com>
2023-12-12 09:56:48 +0530

    Initial commit for Employee Management CRUD project

---
+++ SpringBoot/Project/project06_employee_management_crud/src/main/java/com/project06_employee_management_crud/Project06EmployeeManagementCrudApplication.java
@@ -0,0 +1,13 @@
+package com.project06_employee_management_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project06EmployeeManagementCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project06EmployeeManagementCrudApplication.class, args);
+    }
+
+}
