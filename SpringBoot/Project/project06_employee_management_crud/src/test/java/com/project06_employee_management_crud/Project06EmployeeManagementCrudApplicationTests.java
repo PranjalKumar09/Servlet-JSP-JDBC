c6f35a0b8cc107486d1db359bc63497210b3935b
PranjalKumar09 <coderkumarshukla@gmail.com>
2023-12-19 09:57:44 +0530

    Added test files for E-Notes, E-Commerce, and Employee Management CRUD projects

---
+++ SpringBoot/Project/project06_employee_management_crud/src/test/java/com/project06_employee_management_crud/Project06EmployeeManagementCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project06_employee_management_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project06EmployeeManagementCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
