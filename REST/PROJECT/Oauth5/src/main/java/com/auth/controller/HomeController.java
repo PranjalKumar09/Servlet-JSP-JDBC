0ee4b0a0c58dd5b6528d7884f91fbd01cccd0296
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-01-07 13:35:08 +0530

    Integrated OAuth project: Demonstrating OAuth workflows and implementations.

---
+++ REST/PROJECT/Oauth5/src/main/java/com/auth/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.auth.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public ResponseEntity<?> home(){
+        return ResponseEntity.ok("Hello World");
+    }
+}
---
+++ REST/PROJECT/Oauth5/src/test/java/com/auth/Oauth5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
