9326eeff4d5b94954a9b956178b00d7c5aa01d6c
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-01-13 13:35:08 +0530

    Enhanced ecommerce project: Upgraded features for a scalable Spring Boot solution.

--- SpringBoot/Project/ecommerce/src/main/java/com/ecom/config/SecurityConfig.java
+++ SpringBoot/Project/ecommerce/src/main/java/com/ecom/config/SecurityConfig.java
@@ -4,9 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
@@ -15,8 +13,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-public class SecurityConfig {
+    @Configuration
+    public class SecurityConfig {
     @Autowired
     private AuthSuccessHandlerImpl authSuccessHandler;
     @Autowired
