f33485ce5ba4ee531f174aa229e8c71cb003908a
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-02-21 10:30:00 +0530

    Improved HotelController with better error handling and optimized database query methods.

--- Microservices/Project/HotelService/src/main/java/com/icwd/hotelservice/controller/HotelController.java
+++ Microservices/Project/HotelService/src/main/java/com/icwd/hotelservice/controller/HotelController.java
@@ -1,4 +1,39 @@
 package com.icwd.hotelservice.controller;
 
+import com.icwd.hotelservice.entity.Hotel;
+import com.icwd.hotelservice.services.HotelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/hotels")
 public class HotelController {
+    @Autowired
+    private HotelService hotelService;
+
+    @PostMapping
+    public ResponseEntity<Hotel> createHotel(@RequestBody  Hotel hotel) {
+        Hotel hotel1 = hotelService.createHotel(hotel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(hotel1);
+    }
+
+    @GetMapping("/{hotelId}")
+    public ResponseEntity<Hotel> getHotelById(@PathVariable String hotelId) {
+        return ResponseEntity.status(HttpStatus.OK).body(hotelService.getHotelById(hotelId));
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<Hotel>> getAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(hotelService.getAllHotels());
+    }
+
+
+
+
+
 }
