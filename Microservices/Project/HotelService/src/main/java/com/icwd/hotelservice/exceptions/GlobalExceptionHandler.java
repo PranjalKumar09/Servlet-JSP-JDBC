c618e140075140a71f08340fe2b9c31a150de6d7
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-02-21 11:30:00 +0530

    Updated GlobalExceptionHandler to manage more detailed exception cases across services.

--- Microservices/Project/HotelService/src/main/java/com/icwd/hotelservice/exceptions/GlobalExceptionHandler.java
+++ Microservices/Project/HotelService/src/main/java/com/icwd/hotelservice/exceptions/GlobalExceptionHandler.java
@@ -1,4 +1,24 @@
 package com.icwd.hotelservice.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> notFoundHandler(ResourceNotFoundException e){
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", e.getMessage());
+        response.put("success", false);
+        response.put("status", HttpStatus.NOT_FOUND);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+
+    }
+
 }
