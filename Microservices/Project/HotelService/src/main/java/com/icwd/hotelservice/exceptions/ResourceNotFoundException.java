62bc7d94f8de3eb615cedff3ce95f1dfa7700b7d
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-03-24 11:40:25 +0530

    Added custom exception for handling resource not found errors

--- Microservices/Project/HotelService/src/main/java/com/icwd/hotelservice/exceptions/ResourceNotFoundException.java
+++ Microservices/Project/HotelService/src/main/java/com/icwd/hotelservice/exceptions/ResourceNotFoundException.java
@@ -1,4 +1,11 @@
 package com.icwd.hotelservice.exceptions;
 
-public class ResourceNotFoundException {
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+    public ResourceNotFoundException() {
+        super("Resource not found");
+    }
 }
