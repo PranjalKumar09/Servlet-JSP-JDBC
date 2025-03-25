c5df29dce99ad8b5aec5f793a8ac8a92fd7af21b
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-03-25 11:40:25 +0530

    Implemented HotelReposistory interface for hotel-related data access

--- Microservices/Project/HotelService/src/main/java/com/icwd/hotelservice/reposistory/HotelReposistory.java
+++ Microservices/Project/HotelService/src/main/java/com/icwd/hotelservice/reposistory/HotelReposistory.java
@@ -1,4 +1,10 @@
 package com.icwd.hotelservice.reposistory;
 
-public class HotelReposistory {
+import com.icwd.hotelservice.entity.Hotel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HotelReposistory extends JpaRepository<Hotel, String> {
+
 }
