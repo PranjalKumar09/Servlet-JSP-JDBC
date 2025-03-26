4d3e73256ae29b2b8d22a984551c05ea6199ac98
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-03-26 11:40:25 +0530

    Created HotelService interface for hotel-related business logic

--- Microservices/Project/HotelService/src/main/java/com/icwd/hotelservice/services/HotelService.java
+++ Microservices/Project/HotelService/src/main/java/com/icwd/hotelservice/services/HotelService.java
@@ -1,4 +1,14 @@
 package com.icwd.hotelservice.services;
 
-public class HotelService {
+import com.icwd.hotelservice.entity.Hotel;
+
+import java.util.List;
+
+public interface HotelService {
+
+    Hotel createHotel(Hotel hotel);
+
+    List<Hotel> getAllHotels();
+
+    Hotel getHotelById(String id);
 }
