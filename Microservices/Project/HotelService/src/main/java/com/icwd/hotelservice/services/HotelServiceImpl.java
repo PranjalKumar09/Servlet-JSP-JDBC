0e3bb95d00896de6df17f4613990036f3d36c7ba
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-03-27 11:40:25 +0530

    Implemented HotelService interface with business logic for hotel service

--- Microservices/Project/HotelService/src/main/java/com/icwd/hotelservice/services/HotelServiceImpl.java
+++ Microservices/Project/HotelService/src/main/java/com/icwd/hotelservice/services/HotelServiceImpl.java
@@ -1,4 +1,35 @@
 package com.icwd.hotelservice.services;
 
-public class HotelServiceImpl {
+import com.icwd.hotelservice.entity.Hotel;
+import com.icwd.hotelservice.exceptions.ResourceNotFoundException;
+import com.icwd.hotelservice.reposistory.HotelReposistory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class HotelServiceImpl implements HotelService {
+    @Autowired
+    private HotelReposistory hotelReposistory;
+
+    @Override
+    public Hotel createHotel(Hotel hotel) {
+
+        String hotilId = UUID.randomUUID().toString();
+        hotel.setId(hotilId);
+        return hotelReposistory.save(hotel);
+    }
+
+    @Override
+    public List<Hotel> getAllHotels() {
+        return hotelReposistory.findAll();
+    }
+
+    @Override
+    public Hotel getHotelById(String id) {
+        return hotelReposistory.findById(id).orElseThrow(() -> new ResourceNotFoundException("Hotel not found"));
+    }
 }
