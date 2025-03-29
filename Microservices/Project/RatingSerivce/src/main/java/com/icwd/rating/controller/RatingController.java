260ebacb11d4467262c3cf5f5c8d7097c604d7ca
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-03-29 11:40:25 +0530

    Added RatingController to manage rating API requests

--- Microservices/Project/RatingSerivce/src/main/java/com/icwd/rating/controller/RatingController.java
+++ Microservices/Project/RatingSerivce/src/main/java/com/icwd/rating/controller/RatingController.java
@@ -1,4 +1,42 @@
 package com.icwd.rating.controller;
 
+import com.icwd.rating.entity.Rating;
+import com.icwd.rating.service.RatingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ratings")
 public class RatingController {
+    @Autowired
+    private RatingService ratingService;
+
+    @PostMapping
+    public ResponseEntity<Rating> createRating(@RequestBody Rating rating) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(ratingService.createRating(rating));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Rating>> getAllRating() {
+        return ResponseEntity.status(HttpStatus.OK).body(ratingService.getAll());
+    }
+
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<Rating>> getAllRatingByUserId(@PathVariable String userId) {
+        return ResponseEntity.status(HttpStatus.OK).body(ratingService.getRatingByUserId(userId));
+    }
+
+    @GetMapping("/hotel/{hotelId}")
+    public ResponseEntity<List<Rating>> getAllRatingByHotelId(@PathVariable String hotelId) {
+        return ResponseEntity.status(HttpStatus.OK).body(ratingService.getRatingByHotel(hotelId));
+    }
+
+
+
+
 }
