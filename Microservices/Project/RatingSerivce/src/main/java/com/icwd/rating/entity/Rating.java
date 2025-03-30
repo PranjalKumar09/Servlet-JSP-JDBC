42d56082eb079a257b6273aa97cb4337cff82324
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-03-30 11:40:25 +0530

    Created Rating entity class for storing rating data

--- Microservices/Project/RatingSerivce/src/main/java/com/icwd/rating/entity/Rating.java
+++ Microservices/Project/RatingSerivce/src/main/java/com/icwd/rating/entity/Rating.java
@@ -1,4 +1,25 @@
 package com.icwd.rating.entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import jakarta.persistence.Id;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user_ratings")
 public class Rating {
+    @Id
+    private String ratingId;
+    private String userId;
+    private String hotelId;
+    private int rating;
+    private String feedback;
 }
