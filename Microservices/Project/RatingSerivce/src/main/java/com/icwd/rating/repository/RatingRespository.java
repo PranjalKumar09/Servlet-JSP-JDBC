a81565689ccb64ff9d10d974487695270b365dc4
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-03-31 11:40:25 +0530

    Implemented RatingRespository interface for rating data persistence

--- Microservices/Project/RatingSerivce/src/main/java/com/icwd/rating/repository/RatingRespository.java
+++ Microservices/Project/RatingSerivce/src/main/java/com/icwd/rating/repository/RatingRespository.java
@@ -1,4 +1,13 @@
 package com.icwd.rating.repository;
 
-public class RatingRespository {
+import com.icwd.rating.entity.Rating;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RatingRespository extends JpaRepository<Rating, String> {
+    List<Rating> findAllByUserId(String userId);
+    List<Rating> findAllByHotelId(String hotelId);
 }
