35313f0148c49da77f77b0c4610bba041be5d2d5
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-04-01 11:40:25 +0530

    Created RatingService interface for handling rating business logic

--- Microservices/Project/RatingSerivce/src/main/java/com/icwd/rating/service/RatingService.java
+++ Microservices/Project/RatingSerivce/src/main/java/com/icwd/rating/service/RatingService.java
@@ -1,4 +1,15 @@
 package com.icwd.rating.service;
 
+import com.icwd.rating.entity.Rating;
+
+import java.util.List;
+
 public interface RatingService {
+
+    Rating createRating(Rating rating);
+
+    List<Rating> getRatingByUserId(String userId);
+    List<Rating> getRatingByHotel(String hotelId);
+
+    List<Rating> getAll();
 }
