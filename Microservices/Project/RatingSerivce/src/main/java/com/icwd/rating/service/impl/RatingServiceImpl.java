ba6e04aebd3f2d8af521bf58413654b18d832e59
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-04-02 11:40:25 +0530

    Implemented RatingService interface with business logic for rating

--- Microservices/Project/RatingSerivce/src/main/java/com/icwd/rating/service/impl/RatingServiceImpl.java
+++ Microservices/Project/RatingSerivce/src/main/java/com/icwd/rating/service/impl/RatingServiceImpl.java
@@ -1,4 +1,38 @@
 package com.icwd.rating.service.impl;
 
-public class RatingServiceImpl {
+import com.icwd.rating.entity.Rating;
+import com.icwd.rating.repository.RatingRespository;
+import com.icwd.rating.service.RatingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class RatingServiceImpl implements RatingService {
+    @Autowired
+    private RatingRespository ratingRespository;
+
+    @Override
+    public Rating createRating(Rating rating) {
+        String id = UUID.randomUUID().toString();
+        rating.setRatingId(id);
+        return ratingRespository.save(rating);
+    }
+
+    @Override
+    public List<Rating> getRatingByUserId(String userId) {
+        return ratingRespository.findAllByUserId(userId);
+    }
+
+    @Override
+    public List<Rating> getRatingByHotel(String hotelId) {
+        return ratingRespository.findAllByHotelId(hotelId);
+    }
+
+    @Override
+    public List<Rating> getAll() {
+        return ratingRespository.findAll();
+    }
 }
