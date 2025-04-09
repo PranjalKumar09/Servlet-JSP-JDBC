44e667a2ded7be9a0e70c32c81b57ccf31181183
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-04-09 11:40:26 +0530

    Created User entity class for managing user data

--- Microservices/Project/UserService/src/main/java/com/icwd/userservice/enitity/User.java
+++ Microservices/Project/UserService/src/main/java/com/icwd/userservice/enitity/User.java
@@ -1,4 +1,39 @@
 package com.icwd.userservice.enitity;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+@Table(name = "micro_user")
 public class User {
+
+    @Id
+    @Column(name = "ID")
+    private String userId;
+
+    @Column(name = "NAME", length = 20)
+    private String name;
+
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "ABOUT")
+    private String about;
+
+    @Transient
+    private List<Rating> ratings = new ArrayList<>();
+
+    @Transient
+    private List<Hotel> hotels = new ArrayList<>();
 }
