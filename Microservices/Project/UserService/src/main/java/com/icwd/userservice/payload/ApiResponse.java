83eefab01165ccc67f4c51d11ed603c85990ff50
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-04-12 11:40:26 +0530

    Created ApiResponse class for consistent API response structure

--- Microservices/Project/UserService/src/main/java/com/icwd/userservice/payload/ApiResponse.java
+++ Microservices/Project/UserService/src/main/java/com/icwd/userservice/payload/ApiResponse.java
@@ -1,4 +1,18 @@
 package com.icwd.userservice.payload;
 
+
+import lombok.*;
+import org.springframework.http.HttpStatus;
+
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ApiResponse {
+    private String message;
+    private boolean success;
+    private HttpStatus status;
 }
+
