283685edb47795f903d437f278365ca0c4642cf0
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-04-13 11:40:26 +0530

    Implemented UserReository interface for user data persistence

--- Microservices/Project/UserService/src/main/java/com/icwd/userservice/reposistory/UserReository.java
+++ Microservices/Project/UserService/src/main/java/com/icwd/userservice/reposistory/UserReository.java
@@ -1,4 +1,10 @@
 package com.icwd.userservice.reposistory;
 
-public class UserReository {
+import com.icwd.userservice.enitity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserReository extends JpaRepository<User, String> {
+
 }
