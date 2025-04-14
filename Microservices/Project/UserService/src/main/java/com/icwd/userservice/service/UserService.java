c15245e332588072d401f3fe358e182b327c7843
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-04-14 11:40:26 +0530

    Created UserService interface for user-related business logic

--- Microservices/Project/UserService/src/main/java/com/icwd/userservice/service/UserService.java
+++ Microservices/Project/UserService/src/main/java/com/icwd/userservice/service/UserService.java
@@ -1,4 +1,16 @@
 package com.icwd.userservice.service;
 
+import com.icwd.userservice.enitity.User;
+
+import java.util.List;
+
 public interface UserService {
+
+    User saveUser(User user);
+
+    List<User> findAllUser();
+
+    User getUserById(String id);
+
+
 }
